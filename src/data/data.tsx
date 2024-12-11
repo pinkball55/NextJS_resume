269f337380311f074c856ee805e16edc78674df0
PINKBALL <pinkballmaria55@gmail.com>
2024-12-11 06:48:06 -0800

    updated -- title and description in html page

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'NextJS Resume',
+  description: "Full-stack developer resume",
 };
 
 /**
