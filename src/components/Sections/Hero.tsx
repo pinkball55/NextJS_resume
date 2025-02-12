075f8e09b0c9cd6ba594cb35a0015f4459c70cf3
PINKBALL <pinkballmaria55@gmail.com>
2025-02-12 17:31:57 -0500

    updated -- hero title

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -22,7 +22,7 @@ const Hero: FC = memo(() => {
         />
         <div className="z-10  max-w-screen-lg px-4 lg:px-0">
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-cyan-600/30 p-6 text-center shadow-2xl backdrop-blur-sm">
-            <h1 className="text-4xl font-bold text-neutral-700 sm:text-5xl lg:text-7xl">{name}</h1>
+            <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
             {description}
             <div className="flex gap-x-4 text-neutral-500">
               <Socials />
